160b5ccc075e169427c9dcf09beb9aed53d17519
Bella_work_computer <bellakuhl@thimble.io>
2018-07-26 17:30:36 -0400

    added motor control

--- MorseCode.cpp
+++ MorseCode.cpp
@@ -1,5 +1,5 @@
 /*
-MorseCode.h - Library for writing in Morse code using servo on draw bot.
+MorseCode.h - Library for writing in Morse code using servo on draw bot which also checks the motor speeds.
 Created by Isabella Kuhl July 2018
 Released into public domain
 */
@@ -7,29 +7,65 @@ Released into public domain
 #include "Arduino.h"
 #include "MorseCode.h"
 #include <Servo.h>
+#include "Grove_I2C_Motor_Driver.h"
 
-//Servo myservo;
 
-void MorseCode::init(int pin) {
+#define DOT 75           // Duration of a dot
+#define DASH 375         // Duration of a dash
+#define INNER_PAUSE 125  // Duration of pause between dot and dash in a character 
+#define CHAR_BREAK 250   // Duration of pause between characters
+#define WORD_BREAK 375   // Duration of pause between words (spaces, punctuation, etc)
+
+#define LEFT MOTOR2      // Change if motor orientation is different
+#define RIGHT MOTOR1
+
+// default I2C address is 0x0f
+#define I2C_ADDRESS 0x0f
+
+void countLeft() {
+  lcount++;
+}
+
+void countRight() {
+  rcount++;
+}
+
+void begin() {
+  attachInterrupt(1, countLeft, CHANGE);
+  attachInterrupt(0, countRight, CHANGE);
+}
+
+void MorseCode::init(int pin, int errorValue) {
   myservo.attach(pin);
   _pin = pin;
+  error = errorValue;
+  Motor.begin(I2C_ADDRESS);
+  pinMode(encoderL,INPUT);
+  pinMode(encoderR,INPUT);
+  begin();
+  myservo.write(65);
 }
 
 void MorseCode::dot() {
-  myservo.write(65);
-  delay(250);
   myservo.write(85);
-  delay(250);
+  delay(DOT);
+  myservo.write(65);
+  delay(INNER_PAUSE);
 }
 
 void MorseCode::dash() {
-  myservo.write(65);
-  delay(1000);
   myservo.write(85);
-  delay(250);
+  delay(DASH);
+  myservo.write(65);
+  delay(INNER_PAUSE);
 }
 
-void MorseCode::letter(char c) {
+void MorseCode::translate(String sentence, int motorPower) {
+ leftPower = motorPower;
+ rightPower = motorPower;
+ MorseCode::drive(leftPower, rightPower);
+ for(char& c : sentence) {
+  c = toupper(c);
   switch (c) {
     case 'A':
       MorseCode::dot(); MorseCode::dash();
@@ -60,9 +96,11 @@ void MorseCode::letter(char c) {
     case 'H':
       MorseCode::dot(); MorseCode::dot();
       MorseCode::dot(); MorseCode::dot();
+      MorseCode::drive(leftPower, rightPower);
       break;
     case 'I':
       MorseCode::dot(); MorseCode::dot();
+      MorseCode::drive(leftPower, rightPower);
       break;
     case 'J':
       MorseCode::dot(); MorseCode::dash();
@@ -108,6 +146,7 @@ void MorseCode::letter(char c) {
     case 'U':
       MorseCode::dot();  MorseCode::dot();
       MorseCode::dash();
+      MorseCode::drive(leftPower, rightPower);
       break;
     case 'V':
       MorseCode::dot();  MorseCode::dot();
@@ -128,9 +167,68 @@ void MorseCode::letter(char c) {
     case 'Z':
       MorseCode::dash(); MorseCode::dash();
       MorseCode::dot();  MorseCode::dot();
+      MorseCode::drive(leftPower, rightPower);
       break;
     default:
-      MorseCode::dash();
+      myservo.write(65);
+      MorseCode::drive(leftPower, rightPower);
       break;
+   }
+   delay(CHAR_BREAK);
+}
+}
+
+// function to move robot forward or backward
+void MorseCode::drive(int leftPower, int rightPower) {
+
+  // variables to save tick counts
+  long prevLcount, prevRcount, lDiff, rDiff;
+
+  // Reset the tick counts to zero each time
+  lcount = 0;
+  rcount = 0;
+    
+    Motor.speed(LEFT, leftPower);   
+    Motor.speed(RIGHT, rightPower);
+    
+    // Calculates the difference in previous and current tick counts
+    lDiff = (lcount - prevLcount);
+    rDiff = (rcount - prevRcount);
+  
+    // Saves the current tick count
+    prevLcount = lcount;
+    prevRcount = rcount;
+  
+    // if power is positive...
+    if (leftPower > 0) {
+  
+      // If right motor is slow, it adds the error to the right motor power
+      // and slows down the left motor
+      if (lDiff > rDiff) {
+        leftPower = leftPower - error;
+        rightPower = rightPower + error;
+      }
+      
+      // If left motor is slow, it adds the error to the left motor power
+      // and slows down the right motor
+      else if (lDiff < rDiff) {
+        leftPower = leftPower + error;
+        rightPower = rightPower - error;
+      }
     }
+    // if motor power is negative, do the opposite operations
+    if (leftPower < 0) {
+      if (lDiff > rDiff) {
+        leftPower = leftPower + error;
+        rightPower = rightPower - error;
+      }
+      else if (lDiff < rDiff) {
+        leftPower = leftPower - error;
+        rightPower = rightPower + error;
+      }
+    }
+    Motor.speed(LEFT, leftPower);   
+    Motor.speed(RIGHT, rightPower);
+    // Give the motors a chance to respond   
+    delay(50);
 }
--- MorseCode.h
+++ MorseCode.h
@@ -1,5 +1,5 @@
 /*
-MorseCode.h - Library for writing in Morse code using servo on draw bot.
+MorseCode.h - Library for writing in Morse code using servo on draw bot which also checks the motor speeds.
 Created by Isabella Kuhl July 2018
 Released into public domain
 */
@@ -9,13 +9,31 @@ Released into public domain
 
 #include "Arduino.h"
 #include "Servo.h"
+#include "Grove_I2C_Motor_Driver.h"
+
+void begin();
+void countLeft();
+void countRight();
+static int lcount;
+static int rcount;
+static int leftPower;
+static int rightPower;
+static int error;
 
 class MorseCode {
   public:
-    void init(int pin);
-    void letter(char c);
+    void init(int pin, int errorValue);
+    void translate(String sentence, int motorPower);
     void dot();
     void dash();
+    void drive(int leftPower, int rightPower);
+    void countLeft();
+    void countRight();
+
+    static const int encoderL = 3;
+    static const int encoderR = 2;
+    volatile unsigned int encoderLPos = 1;
+    volatile unsigned int encoderRPos = 0;
   private:
     int _pin;
     Servo myservo;
